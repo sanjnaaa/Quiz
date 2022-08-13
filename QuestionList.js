a36fad732c94cb5d6b3ce2ab39013c523d6cb62d
sanjnaaa <103133300+sanjnaaa@users.noreply.github.com>
2022-08-13 20:34:51 +0530

    Add files via upload

---
+++ QuestionList.js
@@ -0,0 +1,54 @@
+const QuestionList = [
+    {
+        question:"What is the capital of Croatia ?",
+        answerList: [
+           { answer: "Split", isCorrect:false},
+           { answer: "Osijek", isCorrect:false},
+           { answer: "Zagreb", isCorrect:true},
+           { answer: "Rijeka", isCorrect:false},
+          
+        ],
+
+    },
+    {
+        question: "What is the capital of Slovenia ?",
+    answerList:[
+        { answer: "Celje", isCorrect:false},
+        { answer: "Ljubljana", isCorrect:true},
+        { answer: "Maribor", isCorrect:true},
+        { answer: "Koper", isCorrect:false},
+       
+     ],
+
+
+ },
+ {
+    question: "What is the capital of Austria ?",
+    answerList:[
+     {answer: "Graz", isCorrect:false},
+       { answer: "Salzburg", isCorrect:false},
+        { answer: "Innsbruck", isCorrect:false},
+        { answer: "Vienna", isCorrect:true},
+       
+     ],
+
+
+ },
+ {
+    question: "What is the capital of Serbia ?",
+    answerList:[
+        {answer: "Belgrade", isCorrect:true},
+       { answer: "NoviSad", isCorrect:false},
+        { answer: "Niš", isCorrect:false},
+        { answer: "Subotica", isCorrect:false},
+     
+     ],
+
+
+ },
+
+    ];
+    export default QuestionList;
+ 
+
+
